452e01bd206b0f2906d0841ffd14aa7b64fe124f
rimobile1 <icaro.mo1234@gmail.com>
2019-09-04 20:34:21 -0300

    Exercicio 3

---
+++ dist/exercicio3-4.js
@@ -0,0 +1,7 @@
+var numero = 10;
+if (numero % 2 == 0) {
+    console.log("Par");
+}
+else {
+    console.log("Impar");
+}
---
+++ exercicio3-4.ts
@@ -0,0 +1,10 @@
+let numero: number = 10
+
+if(numero %2  == 0 )
+{
+    console.log("Par");
+}
+else
+{
+    console.log("Impar");
+}
