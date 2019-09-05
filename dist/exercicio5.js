369ed97c2f551bcd8f70139cd735127840264e0c
rimobile1 <icaro.mo1234@gmail.com>
2019-09-04 21:48:12 -0300

    Exercicio 5

---
+++ dist/exercicio5.js
@@ -0,0 +1,4 @@
+var comp = 10;
+var larg = 6;
+var vmedia = function (comp, larg) { return (comp * larg); };
+console.log("A area \u00E9 : " + vmedia(comp, larg));
---
+++ exercicio5.ts
@@ -0,0 +1,9 @@
+let comp: number = 10
+let larg: number = 6
+
+
+
+let vmedia = (comp:number,larg:number )  =>  (comp * larg )
+
+
+console.log(`A area é : ${vmedia(comp,larg)}`)
